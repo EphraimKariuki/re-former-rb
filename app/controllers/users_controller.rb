ec9d385c1165d86ce919ccc5ebf8870a42ffb661
ephraimkariuki <ephraimkabuikariuki@gmail.com>
2025-03-31 22:42:23 +0300

    Railsied form using form_with helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    # @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "User added successfully"
