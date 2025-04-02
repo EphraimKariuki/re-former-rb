9d534e7ce47be03adddffe60322580eae515c6c7
ephraimkariuki <ephraimkabuikariuki@gmail.com>
2025-04-02 12:39:38 +0300

    Addition of a edit feature for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     # @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
     @user = User.new(user_params)
@@ -11,6 +15,20 @@ class UsersController < ApplicationController
       redirect_to new_user_path
     else
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "User updated successfully"
+      redirect_to user_path(@user)
+    else
+      render :edit
     end
   end
 
